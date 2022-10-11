4c58926e1e003cf888fb61f3994168d16c7be278
Hagbear <hagbard@toco-tronic.de>
2022-10-11 23:28:21 +0200

    Update and rename check_mk_telegram-notify.sh to check_mk_matrix-notify.sh

--- check_mk_matrix-notify.sh
+++ check_mk_matrix-notify.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
-# Push Notification (using Telegram)
+# Push Notification (using Matrix)
 #
-# Script Name   : check_mk_telegram-notify.sh
-# Description   : Send Check_MK notifications by Telegram
-# Author        : https://github.com/filipnet/checkmk-telegram-notify
+# Script Name   : check_mk_matrix-notify.sh
+# Description   : Send Check_MK notifications by Matrix
+# Orginal Author        : https://github.com/filipnet/checkmk-telegram-notify
 # License       : BSD 3-Clause "New" or "Revised" License
 # ======================================================================================
 
-# Telegram API Token
-# Find telegram bot named "@botfather", type /mybots, select your bot and select "API Token" to see your current token
+# Matrix Server
 if [ -z ${NOTIFY_PARAMETER_1} ]; then
-        echo "No Telegram token ID provided. Exiting" >&2
+        echo "No Matrix server provided. Exiting" >&2
         exit 2
 else
-        TOKEN="${NOTIFY_PARAMETER_1}"
+        HOST="${NOTIFY_PARAMETER_1}"
 fi
 
-# Telegram Chat-ID or Group-ID
-# Open "https://api.telegram.org/bot<YOUR_TOKEN>/getUpdates" inside your Browser and send a HELLO to your bot, refresh side
+# Matrix API Token
 if [ -z ${NOTIFY_PARAMETER_2} ]; then
-        echo "No Telegram Chat-ID or Group-ID provided. Exiting" >&2
+        echo "No Matric token ID provided. Exiting" >&2
         exit 2
 else
-        CHAT_ID="${NOTIFY_PARAMETER_2}"
+        TOKEN="${NOTIFY_PARAMETER_2}"
+fi
+
+# Matrix Chat-ID or Group-ID
+if [ -z ${NOTIFY_PARAMETER_3} ]; then
+        echo "No Matrix Chat-ID or Group-ID provided. Exiting" >&2
+        exit 2
+else
+        ROOM="${NOTIFY_PARAMETER_3}"
 fi
 
 # Privacy settings to anonymize/masking IP addresses
-if [[ ${NOTIFY_PARAMETER_3} == "privacy" ]]; then
+if [[ ${NOTIFY_PARAMETER_4} == "privacy" ]]; then
         # IPv4 IP addresses
         if [ ${NOTIFY_HOST_ADDRESS_4} ]; then
                 slice="${NOTIFY_HOST_ADDRESS_4}"
@@ -54,9 +60,12 @@ if [[ ${NOTIFY_PARAMETER_3} == "privacy" ]]; then
                 # Adjust the output to your privacy needs here (Details in the readme.md)
                 NOTIFY_HOST_ADDRESS_6="${sec1}:${sec2}:${sec3}:${sec4}:ffff:ffff:ffff:${sec8}"
         fi
+
 else
         echo "Invalid privacy parameter, check your Check_MK settings." >&2
 fi
+
+
 
 # Set an appropriate emoji for the current state
 # Feel free to change the emoji to your own taste. This is done by customizing the UTF8 code. Examples here: https://apps.timwhitlock.info/emoji/tables/unicode
@@ -80,24 +89,30 @@ case "${STATE}" in
         ;;
 esac
 
-# Create a MESSAGE variable to send to your Telegram bot
-MESSAGE="${NOTIFY_HOSTNAME} (${NOTIFY_HOSTALIAS})%0A"
-MESSAGE+="${EMOJI} ${NOTIFY_WHAT} ${NOTIFY_NOTIFICATIONTYPE}%0A%0A"
+# Create a MESSAGE variable to send to your Matrix
+MESSAGE="${NOTIFY_HOSTNAME} (${NOTIFY_HOSTALIAS})<br>"
+MESSAGE+="${EMOJI} ${NOTIFY_WHAT} ${NOTIFY_NOTIFICATIONTYPE}<br><br>"
 if [[ ${NOTIFY_WHAT} == "SERVICE" ]]; then
-        MESSAGE+="${NOTIFY_SERVICEDESC}%0A"
-        MESSAGE+="State changed from ${NOTIFY_PREVIOUSSERVICEHARDSHORTSTATE} to ${NOTIFY_SERVICESHORTSTATE}%0A"
-        MESSAGE+="${NOTIFY_SERVICEOUTPUT}%0A"
+        MESSAGE+="${NOTIFY_SERVICEDESC}<br>"
+        MESSAGE+="State changed from ${NOTIFY_PREVIOUSSERVICEHARDSHORTSTATE} to ${NOTIFY_SERVICESHORTSTATE}<br>"
+        MESSAGE+="${NOTIFY_SERVICEOUTPUT}<br>"
 else
-        MESSAGE+="State changed from ${NOTIFY_PREVIOUSHOSTHARDSHORTSTATE} to ${NOTIFY_HOSTSHORTSTATE}%0A"
-        MESSAGE+="${NOTIFY_HOSTOUTPUT}%0A"
+        MESSAGE+="State changed from ${NOTIFY_PREVIOUSHOSTHARDSHORTSTATE} to ${NOTIFY_HOSTSHORTSTATE}<br>"
+        MESSAGE+="${NOTIFY_HOSTOUTPUT}<br>"
 fi
-MESSAGE+="%0AIPv4: ${NOTIFY_HOST_ADDRESS_4} %0AIPv6: ${NOTIFY_HOST_ADDRESS_6}%0A"
+MESSAGE+="<br>IPv4: ${NOTIFY_HOST_ADDRESS_4} <br>IPv6: ${NOTIFY_HOST_ADDRESS_6}<br>"
 MESSAGE+="${NOTIFY_SHORTDATETIME} | ${OMD_SITE}"
 
-# Send message to Telegram bot
-curl -S -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" -d chat_id="${CHAT_ID}" -d text="${MESSAGE}"
+
+# Send message to Matrix
+
+URL="https://${HOST}/_matrix/client/r0/rooms/${ROOM}/send/m.room.message?access_token=${TOKEN}"
+BODY='{"msgtype":"m.text", "body":"'"${MESSAGE}"'","format":"org.matrix.custom.html", "formatted_body":"'"${MESSAGE}"'"}'
+
+curl -S -X POST -d "${BODY}" "${URL}"
+
 if [ $? -ne 0 ]; then
-        echo "Not able to send Telegram message" >&2
+        echo "Not able to send Matric message" >&2
         exit 2
 else
         exit 0
